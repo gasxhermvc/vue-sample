17b49a4a234c15124007181a8c8bc90a2b759022
Tanasit Auewongaree <006006@cdg.co.th>
2021-06-15 18:42:21 +0700

    add first-app

---
+++ first-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Vue App Component</h1>
+    <div id="my-data">ชื่อ {{ name }} อายุ {{ age }}</div>
+    {{ myProfile() }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "tanasit auewongaree",
+      age: 28,
+    };
+  },
+  methods: {
+    myProfile() {
+      return `My name: ${this.name}, age: ${this.age}`;
+    },
+    method1() {
+      return "First result";
+    },
+    method2(id) {
+      return `Second result is: ${id}`;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ first-app/src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="content">
+    <h1>This is from my component</h1>
+    <p>Hello my name {{ myPropName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      myPropName: "Tanasit Auewongaree",
+    };
+  },
+};
+</script>
+
+<style>
+#content {
+  text-align: center;
+  color: blue;
+}
+</style>
