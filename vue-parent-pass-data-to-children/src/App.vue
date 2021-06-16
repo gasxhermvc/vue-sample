411f7279b1af753ffe3bf5cd0621aa1690ecbdf2
Tanasit Auewongaree <006006@cdg.co.th>
2021-06-16 13:53:18 +0700

    add vue-parent-pass-data-to-childrend

---
+++ vue-parent-pass-data-to-children/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <UserLists :type="type"></UserLists>
+    <button type="button" @click="changeViewer">Change viewer</button>
+  </div>
+</template>
+
+<script>
+import UserLists from "@/components/UserLists";
+
+export default {
+  name: "App",
+  components: {
+    UserLists,
+  },
+  data() {
+    return {
+      type: "",
+    };
+  },
+  methods: {
+    changeViewer() {
+      if (this.type == "card") this.type = "";
+      else this.type = "card";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-parent-pass-data-to-children/src/components/UserDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-col :sm="sm" :lg="lg" class="mb-3 pointer" @click="onSelected(user)">
+    <b-card :class="{ selected: user.selected }">
+      <b-card-text class="present text-left">
+        <b-col> ชื่อจริง: {{ user.name }} </b-col>
+        <b-col> ชื่อผู้ใช้งาน: {{ user.username }} </b-col>
+      </b-card-text>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+      default: null,
+    },
+    sm: {
+      type: Number,
+      default: 6,
+    },
+    lg: {
+      type: Number,
+      default: 4,
+    },
+  },
+  methods: {
+    onSelected(user) {
+      user.selected = !user.selected;
+      this.$emit("updateSelected", user);
+    },
+  },
+};
+</script>
+
+<style>
+.present {
+  white-space: pre-line;
+  word-break: break-all;
+}
+.pointer {
+  cursor: pointer;
+}
+.selected {
+  border-color: rgb(57, 57, 177);
+}
+</style>
